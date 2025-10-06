052a5cb0e462c24ee0d8ef3697f71b78ce599d9f
Jhon Jimenez <u20231c873@upc.edu.pe>
2025-10-05 21:08:37 -0500

    Commit entities:
    Commit de entidades.
    
    #1

---
+++ src/main/java/com/upc/festisolutions/entities/Reporteproveedor.java
@@ -0,0 +1,33 @@
+package com.upc.festisolutions.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reporteproveedor")
+public class Reporteproveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idreporteproveedor", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "idproveedor", nullable = false)
+    private Proveedor proveedor;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "descripcion", nullable = false, length = 100)
+    private String descripcion;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "idgananciaevento")
+    private Gananciaevento gananciaevento;
+}
