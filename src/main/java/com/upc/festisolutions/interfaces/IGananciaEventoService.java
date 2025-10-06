6620dcf9f9f36028f695da04d76cd5bb792e2bf3
Cesar <u20231e202@upc.edu.pe>
2025-10-05 21:36:07 -0500

    -

--- src/main/java/com/upc/festisolutions/interfaces/IGananciaEventoService.java
+++
@@ -1,7 +0,0 @@
-package com.upc.festisolutions.interfaces;
-
-import com.upc.festisolutions.dto.GananciaEventoDTO;
-
-public interface IGananciaEventoService {
-    public GananciaEventoDTO registrarGananciaEvento(GananciaEventoDTO gananciaEventoDTO);
-}
