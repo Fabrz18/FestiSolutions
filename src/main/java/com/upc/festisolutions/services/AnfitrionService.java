457de2c4106aec6b1c735b3a0654c8643e8e9415
Thiago Vidal <u202310919@upc.edu.pe>
2025-10-05 21:28:38 -0500

    Creacion de capa Services para Celebra proyect

---
+++ src/main/java/com/upc/festisolutions/services/AnfitrionService.java
@@ -0,0 +1,70 @@
+package com.upc.festisolutions.services;
+
+import com.upc.festisolutions.dto.AnfitrionDTO;
+import com.upc.festisolutions.entities.Anfitrion;
+import com.upc.festisolutions.interfaces.IAnfitrionService;
+import com.upc.festisolutions.repository.AnfitrionRepository;
+import com.upc.festisolutions.security.entities.Role;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AnfitrionService implements IAnfitrionService {
+    @Autowired
+    private AnfitrionRepository anfitrionRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public AnfitrionDTO registrarAnfitrion(AnfitrionDTO anfitrionDTO) {
+        if (anfitrionDTO.getId() == null) {
+            Anfitrion anfitrion = modelMapper.map(anfitrionDTO, Anfitrion.class);
+            anfitrion.setEstado(true);
+            Role rol = new Role();
+            rol.setId(4L);
+            rol.setName("ROLE_ANFITRION");
+            anfitrion.setRole(rol);
+            Anfitrion guardado = anfitrionRepository.save(anfitrion);
+            return modelMapper.map(guardado, AnfitrionDTO.class);
+        }
+        return null;
+    }
+
+    @Override
+    public void eliminarAnfitrion(Integer id) {
+        Anfitrion anfitrion = anfitrionRepository.findById(id).orElse(null);
+        if (anfitrion != null){
+            anfitrion.setEstado(false);
+            anfitrionRepository.save(anfitrion);
+        }
+    }
+    @Override
+    public AnfitrionDTO actualizarAnfitrion(AnfitrionDTO anfitrionDTO){
+        if (anfitrionDTO.getId() != null){
+            Anfitrion anfitrion = modelMapper.map(anfitrionDTO, Anfitrion.class);
+            anfitrion.setEstado(true);
+            Role rol = new Role();
+            rol.setId(4L);
+            rol.setName("ROLE_ANFITRION");
+            anfitrion.setRole(rol);
+            anfitrionRepository.save(anfitrion);
+            return modelMapper.map(anfitrion, AnfitrionDTO.class);
+        }
+        return null;
+    }
+    @Override
+    public List<AnfitrionDTO> listarAnfitrion(){
+        List<Anfitrion> anfitriones = anfitrionRepository.findByEstadoTrue();
+        return anfitriones.stream().map(anfitrion  -> modelMapper.map(anfitrion, AnfitrionDTO.class)).toList();
+    }
+    @Override
+    public AnfitrionDTO buscarAnfitrionPorId(Integer id){
+        Anfitrion anfitrion = anfitrionRepository.findById(id).orElse(null);
+        return modelMapper.map(anfitrion, AnfitrionDTO.class);
+    }
+}
