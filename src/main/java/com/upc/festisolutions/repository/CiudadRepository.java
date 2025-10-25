61917f40c11c7f44919565760989f538066fc872
Jhon Jimenez <u20231c873@upc.edu.pe>
2025-10-25 12:39:22 -0500

    Implementación #2 de landing page

--- src/main/java/com/upc/festisolutions/repository/CiudadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.festisolutions.repository;
-
-import com.upc.festisolutions.entities.Ciudad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CiudadRepository extends JpaRepository<Ciudad, Integer> {
-}
--- src/main/java/com/upc/festisolutions/repository/DistritoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.festisolutions.repository;
-
-import com.upc.festisolutions.entities.Distrito;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DistritoRepository extends JpaRepository<Distrito, Integer> {
-}
